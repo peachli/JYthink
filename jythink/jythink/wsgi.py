de0f9a2b65a5ce005bf3ec953fbca49325ad774b
litao <litao@arraynetworks.com>
2017-12-30 00:53:22 +0800

    deploy django/uwsgi/nginx

---
+++ jythink/jythink/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for jythink project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+sys.path.append('/usr/lib/python2.7/site-packages')  
+sys.path.append('/usr/lib64/python2.7/site-packages') 
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jythink.settings")
+
+application = get_wsgi_application()
